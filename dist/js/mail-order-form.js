803ffe4d9972f306c0d9a90fee9d36e0ca40957c
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-04-25 16:02:28 -0700

    fix order form, port from jquery

--- dist/js/mail-order-form.js
+++ dist/js/mail-order-form.js
@@ -1,21 +1,38 @@
-$(function() {
-  var form = $('form');
-  form.on('submit', (e)=>{
-    e.preventDefault();
-    var data = form.serialize();
-    var productID = data[1].value;
-    var quantity = data[2].value;
-    var firstname = data[3].value;
-    var lastname = data[4].value;
-    var phone = data[5].value;
-    var address = data[6].value;
-    var shipping = data[7].value;
-    var cc1 = data[8].value;
-    var cc2 = data[9].value;
-    var cc3 = data[10].value;
-    var cc4 = data[11].value;
-    var expiry = data[12].value;
-    //FIXME fuck man, I don't know how to get these variables to come out right
+var form = document.getElementById('form-order-form');
+
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  var data = new FormData(form);
+  var productID = data.get('product-id');
+  var quantity = data.get('quantity');
+  var firstname = data.get('firstname');
+  var lastname = data.get('lastname');
+  var phone = data.get('phone-number');
+  var address = data.get('address');
+  var shipping = data.get('shipping');
+  var cc1 = data.get('creditCard1');
+  var cc2 = data.get('creditCard2');
+  var cc3 = data.get('creditCard3');
+  var cc4 = data.get('creditCard4');
+  var expiry = data.get('expiry');
+
+  function validate() {
+    var errors = [];
+    if(!phone.match(/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/)){
+      errors.push("invalid phone number!");
+    }
+    var validAddress = address.trim().length > 0;
+    if(!validAddress){
+      errors.push("shipping address can't be blank!");
+    }
+    if(!expiry.match(/^20(1[89]|[2-9][0-9])-\d\d$/)){
+      errors.push("credit card expiration dates must be in the future");
+    }
+    return errors;
+  }
+
+  var errors = validate();
+  if ( errors.length === 0 ) {
     var subject = 'Order From ' + firstname + ' ' + lastname;
     var body = 'Order For: ' + firstname + ' ' + lastname + '\n' +
       'Product ID: ' + productID + '\n' +
@@ -25,8 +42,10 @@ $(function() {
       + address + '\n' +
       'Credit Card: '  + cc1 + '-' + cc2 + '-' + cc3 + '-' + cc4 + '\n' +
       'Expiration Date: ' + expiry + '\n';
-    var addr = form.attr('action');
+    var addr = form.getAttribute('action');
     console.log(body)
-    window.open(addr+"?"+data, '_self')
-  });
-})
+    window.open(`${addr}?subject=${subject}&body=${escape(body)}`, '_self');
+  } else {
+    alert(errors.join('\n'));
+  }
+});
