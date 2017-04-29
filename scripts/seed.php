c4f72e6eba4388d9c8c27b797aec3316f9e4ec41
inf124grp08 <inf124grp08@andromeda-16.ics.uci.edu>
2017-04-28 20:23:46 -0700

    seed script working

--- scripts/seed.php
+++ scripts/seed.php
@@ -1,5 +1,6 @@
 <?php
 $config = json_decode(file_get_contents("config/db.json"), true);
+$data = json_decode(file_get_contents("data.json"), true);
 try {
   $db = new PDO(
     "mysql:dbname=".$config['db'].";host=".$config['host'],
@@ -7,13 +8,48 @@ try {
   $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
   $table="categories";
+  $db->exec("DROP TABLE IF EXISTS $table;");
+  print("Dropped table $table if it existed.\n");
+
   $sql ="CREATE TABLE IF NOT EXISTS $table(
-    ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR( 50 ) PRIMARY KEY,
     label VARCHAR( 255 ) NOT NULL, 
     description TEXT NOT NULL,
     image VARCHAR( 255 ) NOT NULL);";
   $db->exec($sql);
-  print("Created $table Table.\n");
+  print("Created table $table.\n");
+
+  foreach ($data['categories'] as $name => $cat) {
+    $label = $cat['label'];
+    $desc = $cat['description'];
+    $img = $cat['image'];
+    $sql = "INSERT INTO $table (name, label, description, image) VALUES ('$name', '$label', '$desc', '$img')";
+    $db->exec($sql);
+  }
+
+  $table="products";
+  $db->exec("DROP TABLE IF EXISTS $table;");
+  print("Dropped table $table if it existed.\n");
+
+  $sql ="CREATE TABLE IF NOT EXISTS $table(
+    id INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
+    category_id VARCHAR( 50 ) NOT NULL,
+    price numeric(15,2) NOT NULL, 
+    brand VARCHAR( 50 ) NOT NULL,
+    description TEXT NOT NULL,
+    image VARCHAR( 255 ) NOT NULL);";
+  $db->exec($sql);
+  print("Created table $table.\n");
+
+  foreach ($data['products'] as $prod) {
+    $cat = $prod['category'];
+    $desc = $prod['description'];
+    $img = $prod['image'];
+    $brand = $prod['brand'];
+    $price = $prod['price'];
+    $sql = "INSERT INTO $table (category_id, price, brand, description, image) VALUES ('$cat', '$price', \"$brand\", \"$desc\", '$img');";
+    $db->exec($sql);
+  }
 
 } catch(PDOException $e) {
   echo $e->getMessage();//Remove or change message in production code
